b71da9232543b505bec5bfe95de960dc210ea5fb
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-06-02 21:44:30 -0300

    feat: adding menu bar [WIP]

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -24,7 +24,7 @@ import org.apache.pdfbox.multipdf.PDFMergerUtility;
  */
 public class MainFrame extends javax.swing.JFrame {
     
-    Image searchIcon = new ImageIcon(System.getProperty("user.dir")+ "/src/main/java/assets/search.png").getImage();
+    ImageIcon searchIcon = new ImageIcon(getClass().getResource("/search.png") );
     
     private DefaultListModel<String> availableSongsListModel;
     private DefaultListModel<String> addedSongsListModel;
@@ -37,7 +37,7 @@ public class MainFrame extends javax.swing.JFrame {
         initComponents();
         this.setDefaultModelToListsComponents();
         this.listPdfFiles();
-        this.lblSearchIcon.setIcon(new ImageIcon(searchIcon));
+        this.lblSearchIcon.setIcon(searchIcon);
     }
     
     private void listPdfFiles() {
@@ -80,6 +80,9 @@ public class MainFrame extends javax.swing.JFrame {
         bntMergePdfs = new javax.swing.JButton();
         lblSearchIcon = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("PDF Together");
@@ -88,7 +91,7 @@ public class MainFrame extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(49, 56, 59));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("/Users/juliomiguel/Documents/Me/pdf-together/src/main/java/assets/PDF Together.png")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/PDF Together.png"))); // NOI18N
         jLabel1.setText("jLabel1");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -200,7 +203,7 @@ public class MainFrame extends javax.swing.JFrame {
                         .addGap(25, 25, 25)
                         .addComponent(lblAvailablePdfs))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgLayout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGap(8, 8, 8)
                         .addComponent(lblAddedPdfs, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -213,6 +216,19 @@ public class MainFrame extends javax.swing.JFrame {
                     .addComponent(lblSearchIcon, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(37, 37, 37))
         );
+
+        jMenu1.setText("PDFs Files");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("About");
+        jMenu2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu2MouseClicked(evt);
+            }
+        });
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -229,7 +245,7 @@ public class MainFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void lblSearchIconMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblSearchIconMouseClicked
-        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(null, "Funcionalidade indisponível nesta release");
     }//GEN-LAST:event_lblSearchIconMouseClicked
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
@@ -297,6 +313,11 @@ public class MainFrame extends javax.swing.JFrame {
 
         }
     }//GEN-LAST:event_availableSongsValueChanged
+
+    private void jMenu2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu2MouseClicked
+        AboutFrame aboutFrame = new AboutFrame();
+        aboutFrame.setVisible(true);
+    }//GEN-LAST:event_jMenu2MouseClicked
     
     private void showContextMenu(MouseEvent e) {
         JPopupMenu popupMenu = new JPopupMenu();
@@ -316,6 +337,9 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JPanel bg;
     private javax.swing.JButton bntMergePdfs;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
