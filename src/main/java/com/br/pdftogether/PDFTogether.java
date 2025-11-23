6c4e5331378896d2bcec4702917546f066db55fd
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-23 11:17:59 -0300

    feat: Apply FlatDarkLaf theme to the application, removing hardcoded UI colors

--- src/main/java/com/br/pdftogether/PDFTogether.java
+++ src/main/java/com/br/pdftogether/PDFTogether.java
@@ -9,6 +9,11 @@ import com.br.pdftogether.gui.SelectPDFSFolderFrame;
 public class PDFTogether {
 
     public static void main(String[] args) {
+        try {
+            javax.swing.UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
